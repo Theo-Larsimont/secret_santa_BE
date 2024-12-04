976cde58dcbcaa289ea28d726423041cf6d2257f
Théo Larsimont <theolarsimont@proton.me>
2024-12-04 20:35:49 +0100

    initial commit

---
+++ src/main/java/com/example/secret_santa_be/SecretSantaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.secret_santa_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSantaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretSantaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secret_santa_be/SecretSantaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secret_santa_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSantaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
